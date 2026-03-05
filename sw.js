0cd2554d9d14997d5922030040d4dfc684321c50
losha24 <losha24@gmail.com>
2026-03-06 01:26:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,10 @@
-const CACHE_NAME = 'carton-v1.1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
-];
+const CACHE_NAME = 'carton-v7.2.1';
+const assets = ['./', './index.html', './manifest.json'];
 
-// התקנה ושמירת קבצים בזיכרון
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
 });
 
-// הפעלה וניקוי קבצים ישנים
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// שליפת נתונים מהזיכרון במידה ואין אינטרנט
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
