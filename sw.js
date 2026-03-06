e4bbe159e8a0eb394d242b49c2007b992fb42d32
losha24 <losha24@gmail.com>
2026-03-06 20:45:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,16 @@
 const CACHE_NAME = 'math-pwa-v2';
-const ASSETS = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon.png'
-];
+const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
