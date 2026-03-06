85325942e507480dc0ba47bcb242391f397ca399
losha24 <losha24@gmail.com>
2026-03-06 20:33:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const cacheName = 'math-kids-v1.0.2';
-const assets = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.png'];
+const CACHE_NAME = 'math-pwa-v2';
+const ASSETS = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon.png'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
