326229f28d92f10c7fd38018fd8ad2ab2ce806e4
losha24 <losha24@gmail.com>
2026-03-06 19:24:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'math-kids-v1-0-1';
+const cacheName = 'math-pwa-v1.0.1';
 const assets = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
