f7c8afb753b5ce169da4086a96639e1c003bd6e1
losha24 <losha24@gmail.com>
2026-03-07 13:40:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
 const CACHE_NAME = 'math-pro-v2.1';
-const ASSETS = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './manifest.json',
-    './icon.PNG'
-];
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.PNG'];
 
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+self.addEventListener('install', (e) => { 
+    self.skipWaiting(); 
+    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); 
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((ks) => {
-        return Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)));
-    }));
-    return self.clients.claim();
+self.addEventListener('activate', (e) => { 
+    e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k))))); 
+    return self.clients.claim(); 
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch', (e) => { 
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request))); 
 });
