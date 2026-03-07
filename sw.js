b79765f3e830492794fffd8d6c55bd3777834d22
losha24 <losha24@gmail.com>
2026-03-07 12:04:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-const CACHE_NAME = 'math-pwa-v1.0.3';
-const ASSETS = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icon.PNG'
-];
+const CACHE_NAME = 'math-pro-v1.1.0';
+const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json', './icon.PNG'];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+  e.waitUntil(caches.keys().then((ks) => Promise.all(ks.map((k) => k !== CACHE_NAME && caches.delete(k)))));
   self.clients.claim();
 });
 
