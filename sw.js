e6b1c5968f841b3efd05663294273acaf306d793
losha24 <losha24@gmail.com>
2026-03-07 14:17:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'math-pro-v7.2.2';
+const CACHE_NAME = 'math-pro-v3.0.0';
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.PNG'];
 
 self.addEventListener('install', (e) => { 
